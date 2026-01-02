d8019da730f2372469ec3a6309ae1a35fc11b46e
Nauman Munir <mrnomanmunir@gmail.com>
2026-01-02 07:28:33 +0000

    Refactor ft_write function for improved readability by aligning comments and maintaining consistent formatting

--- ft_write.s
+++ ft_write.s
@@ -8,18 +8,18 @@ section .text
 	extern __errno_location
 
 ft_write:
-	mov		rax, 1				; syscall number for write
-	syscall						; Perform syscall
-	cmp		rax, 0				; Check for error (negative return)
-	jl		.error				; If negative, handle error
-	ret							; Return bytes written
+	mov		rax, 1
+	syscall
+	cmp		rax, 0
+	jl		.error
+	ret
 
 .error:
-	neg		rax					; Convert negative error code to positive errno
-	mov		rdi, rax			; Save errno value
-	push	rdi					; Preserve errno value on stack
-	call	__errno_location wrt ..plt	; Get pointer to errno
-	pop		rdi					; Restore errno value
-	mov		[rax], edi			; Set errno
-	mov		rax, -1				; Return -1
+	neg		rax
+	mov		rdi, rax
+	push	rdi
+	call	__errno_location wrt ..plt
+	pop		rdi
+	mov		[rax], edi
+	mov		rax, -1
 	ret
